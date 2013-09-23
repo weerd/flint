b618a4305f2144b2ab43d88f5aa3a325041e92d4
Diego Lorenzo <diego@bureaublank.com>
2013-09-23 12:15:38 -0400

    The img folder renamed to images.

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@
 http_path = "/"
 css_dir = "assets/css"
 sass_dir = "assets/sass"
-images_dir = "assets/img"
+images_dir = "assets/images"
 javascripts_dir = "assets/js"
 fonts_dir = "assets/fonts"
 
